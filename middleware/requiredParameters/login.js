7380b6b90545bb358e40703b5775ec8efd0ef9be
Kadir Can KARADEMİR <50851192+CanKadirKarademir@users.noreply.github.com>
2022-06-21 02:15:32 +0300

    refactor: refactor login validator

--- middleware/requiredParameters/login.js
+++ middleware/requiredParameters/login.js
@@ -2,6 +2,7 @@ const joi = require("joi");
 
 module.exports = {
   "/login": {
+    //test
     POST: joi.object().keys({
       body: joi.object({
         EmailAddress: joi.string().max(256).required(),
